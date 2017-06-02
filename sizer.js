3030a5c8bec4c70883ee55d2cfd6ba23071812c8
Kevin Smith <ksjazzguitar@yahoo.com>
2017-06-02 10:53:12 -0700

    inital

---
+++ sizer.js
@@ -0,0 +1,29 @@
+// install what we need
+
+var express = require('express');
+var multer  = require('multer');
+
+var upload = multer( { dest: 'uploads/' });
+
+var isLocal = (process.argv[2]=="local");
+
+var port = (!isLocal)?process.env.PORT:1337;
+var host = (!isLocal)?process.env.IP:'127.0.0.1';
+
+var app = express();
+
+// middleware
+
+app.get('/', function(req, res, next) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/upload', upload.single('file'), function(req, res) {
+  return res.json({ size: req.file.size } );
+});
+
+// launch app
+
+var localStr = (isLocal)?"locally ":"";
+app.listen(port, host, function() { console.log("sizer server is running " + localStr + "on port " +
+        port + " and host " + host + " ...")});
